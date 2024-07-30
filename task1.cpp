493ee2400132227269fc7fbff2266c9330646998
Ferum4k <105701202+Ferum4k@users.noreply.github.com>
2024-07-30 17:25:39 +0300

    Исправление ошибок

--- task1.cpp
+++ task1.cpp
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <TXLib.h>
 #include <math.h>
-/*Решалка квадратных уравнений */
+/*quadratic equation solver */
+
+const double eps = 1e-5
 
 int main()
 {
-    double eps = pow(10,-5);
     double x = 0.0, x1 = 0.0, x2 = 0.0;
-    int a = 0, b = 0, c = 0;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    scanf("%d",&c);
+    double a = 0, b = 0, c = 0;
+
+    scanf("%f","%f","%f",&a, &b, &c);
+
     double D = pow(b,2) - 4 * a * c;
+
     if (a == 0)
         x = -c/b;
         printf("%f",x);
@@ -19,15 +21,19 @@ int main()
     {
         if (b == 0)
             D = -4*a*c;
+
         if (c == 0)
             D = pow(b, 2);
+
         if (D < 0)
-            printf("Нет корней");
+            printf("No answer, unlucky");
+
         if (abs((D - 0)) < eps)
         {
             x = (-b) / (2*a) ;
             printf("%f",x);
         }
+
         if (D > 0)
         {
             x1 = (-b + sqrt(D)) / (2*a);
